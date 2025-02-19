c76d82b6854d5590a7f86a4921cc7e44b31de8c4
Ryan Ramsey <rramsey1115@gmail.com>
2025-02-19 13:39:37 -0600

    HP Companion name change

---
+++ app/lib/definitions.js
@@ -0,0 +1,11 @@
+
+// definitions for data
+// manually defining for now
+
+export const User = {
+  id: String,
+  name: String,
+  email: String,
+  password: String,
+}
+
---
+++ app/lib/placeholder-data.js
@@ -0,0 +1,19 @@
+
+// placeholder data for seeding database initially
+
+const users = [
+  {
+    id: "410544b2-4001-4271-9855-fec4b6a6442a",
+    name: "User",
+    email: "user@nextmail.com",
+    password: "123456",
+  },
+];
+
+const houses = [
+
+]
+
+const savedHouses = [
+
+]
