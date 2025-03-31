74fb055d1c59e8d35d69f8eefc4dcd681ad0eabf
Ryan Ramsey <rramsey1115@gmail.com>
2025-03-31 15:35:00 -0500

    displaying character data

--- app/characters/page.jsx
+++ app/characters/page.jsx
@@ -1,22 +1,60 @@
+"use client"
 
-// function to retrieve data from api
-const getCharacters = async () => {
-    const response = await fetch("http://localhost:3000/api/characters", {
-        method: "GET"
-    })
-    return response.json();
-}
+import { useEffect, useState } from "react";
 
-const Characters = async () => {
-    const characters = await getCharacters();
 
-    // console.log("characters", characters[2]);
 
-    return (
-        <>
-            <h1>Characters Page</h1>
-        </>
-    );
-}
+const Characters = () => {
+    const [characters, setCharacters] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const getAndSetCharacters = async () => {
+      const res = await fetch("http://localhost:3000/api/characters");
+      const data = await res.json();
+      setCharacters(data);
+      setLoading(false);
+    };
+
+  useEffect(() => {
+    getAndSetCharacters();
+  }, []);
+
+  if (loading) {
+    return <h1>...Loading</h1>;
+  }
+
+  return (
+    <>
+      <h1>Characters Page</h1>
+      <section>
+        <table>
+          <thead>
+            <tr>
+              <td>Name</td>
+              <td>Wizard</td>
+              <td>Hogwarts Student</td>
+              <td>House</td>
+            </tr>
+          </thead>
+          <tbody>
+            {characters.map((char) => {
+              return (
+                <tr
+                  key={char.id}
+                  className="cursor-pointer hover:bg-yellow-600 hover:text-black"
+                >
+                  <td>{char.name}</td>
+                  <td>{char.wizard ? "Yes" : "No"}</td>
+                  <td>{char.hogwartsstudent ? "Yes" : "No"}</td>
+                  <td>{char.house ? char.house : ""}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </section>
+    </>
+  );
+};
 
 export default Characters;
