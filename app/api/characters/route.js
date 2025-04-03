0baf621524b811a925f4658559f2a95588debeee
Ryan Ramsey <rramsey1115@gmail.com>
2025-04-02 20:02:18 -0500

    updated characters get() endpoint to accomodate filters

--- app/api/characters/route.js
+++ app/api/characters/route.js
@@ -2,11 +2,37 @@ import { db } from "@/lib/connection/db.js";
 import { characters } from "../../../lib/definitions.js";
 import { NextResponse } from "next/server";
 
+// get characters endpoint which accomodates for filters which are passed in as numbers
 export async function GET(req) {
   try {
+    const { searchParams } = new URL(req.url);
+    const filter = searchParams.get("filter");
+
+    //get all characters regarless of filter or not
     const allCharacters = await db.select().from(characters);
-    return NextResponse.json(allCharacters);
+
+    if (filter == 1) {
+      // if the filter is 1, sort by last name A-Z
+      allCharacters.sort((a, b) => {
+        const lastNameA = a.name.split(" ").slice(-1)[0];
+        const lastNameB = b.name.split(' ').slice(-1)[0];
+        return lastNameA.localeCompare(lastNameB);
+      });
+      return NextResponse.json(allCharacters, {status: 200});
+    } else if (filter == 2) {
+      // if the filter is 1, sort by last name A-Z
+      allCharacters.sort((a, b) => {
+        const lastNameA = a.name.split(" ").slice(-1)[0];
+        const lastNameB = b.name.split(' ').slice(-1)[0];
+        return lastNameB.localeCompare(lastNameA);
+      });
+      return NextResponse.json(allCharacters);
+    }
+
+    // return characters array regarless of filter or not
+    return NextResponse.json(allCharacters, {status: 200});
   } catch (error) {
+    // return error from anywhere in endpoint
     console.error("Error in get characters:", error);
     return NextResponse.json({ error: "Failed to fetch characters" }, { status: 500 });
   }
--- app/characters/page.jsx
+++ app/characters/page.jsx
@@ -5,27 +5,29 @@ import { useEffect, useState } from "react";
 const Characters = () => {
     const [characters, setCharacters] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [filter, setFilter] = useState(1);
+    const [filter, setFilter] = useState(0);
 
     const getAndSetCharacters = async () => {
-        const res = await fetch("/api/characters");
-        const data = await res.json();
-        
-        if(filter == 2) {
-            data.sort((a, b) => a.name.split(" ")[1] - b.name.split(" ")[1])
-        } else if(filter == 3 ) {
-            data.sort((a, b) => b.name.split(" ")[1] - a.name.split(" ")[1])
-        } 
-
-        setCharacters(data);
+        setLoading(true);
+        if (filter == 1 || filter == 2) {
+            // if filter A-Z by last
+            console.log("filter A-Z")
+            const res = await fetch(`/api/characters?filter=${parseInt(filter)}`) 
+            const data = await res.json();
+            setCharacters(data);
+        } else {
+            // get all characters in default order
+            console.log("filter default");
+            const res = await fetch("/api/characters");
+            const data = await res.json();
+            setCharacters(data);
+        }
+        // whatever res we get from if statements, set to json data
         setLoading(false);
     };
 
     const handleSelect = async (e) => {
-        setLoading(true);
         setFilter(e.target.value);
-        await getAndSetCharacters();
-        setLoading(false);
     }
 
     useEffect(() => {
@@ -38,25 +40,24 @@ const Characters = () => {
 
     return (
         <>
-        <header>
-            <h1 className="text-3xl h-10 my-4">Characters</h1>
-            <select id="filter-dropdown" name="filter-dropdown" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                <option defaultChecked>Order By</option>
-                <option value={1}>Default</option>
-                <option value={2}>Last A-Z</option>
-                <option value={3}>Last Z-A</option>
-            </select>
-        </header>
+            <header>
+                <h1 className="text-3xl h-10 my-4">Characters</h1>
+                <select value={filter} onChange={handleSelect} id="filter-dropdown" name="filter-dropdown" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option value={0}>Order By</option>
+                    <option value={1}>Last A-Z</option>
+                    <option value={2}>Last Z-A</option>
+                </select>
+            </header>
             <section className="border-t-2 border-stone-300 m-1 p-1">
-                        {characters.map((char) => {
-                            return (<div
-                                key={char.id}
-                                className="cursor-pointer hover:bg-yellow-500 hover:text-black w-3/4"
-                            >
-                                <h5 className="text-lg font-thin">{char.name}</h5>
-                            </div>);
-                        })}
-                
+                {characters.map((char) => {
+                    return (<div
+                        key={char.id}
+                        className="cursor-pointer hover:bg-yellow-500 hover:text-black w-3/4"
+                    >
+                        <h5 className="text-lg font-thin">{char.name}</h5>
+                    </div>);
+                })}
+
             </section>
         </>
     );
